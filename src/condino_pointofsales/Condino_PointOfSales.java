22521c252bcaa721b62009e32ad72852b2fef507
Almac2x <53673507+Almac2x@users.noreply.github.com>
2019-08-04 19:23:56 +0800

    Initial Commit

---
+++ src/condino_pointofsales/Condino_PointOfSales.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package condino_pointofsales;
+
+/**
+ *
+ * @author bland
+ */
+public class Condino_PointOfSales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
