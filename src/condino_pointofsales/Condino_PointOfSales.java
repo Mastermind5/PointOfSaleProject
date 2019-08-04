87251647457f3698b92383898c6c641802d7c842
Almac2x <53673507+Almac2x@users.noreply.github.com>
2019-08-04 19:30:30 +0800

    Version1

--- src/condino_pointofsales/Condino_PointOfSales.java
+++ src/condino_pointofsales/Condino_PointOfSales.java
@@ -5,6 +5,8 @@
  */
 package condino_pointofsales;
 
+import java.util.Scanner;
+
 /**
  *
  * @author bland
@@ -15,7 +17,48 @@ public class Condino_PointOfSales {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+          
+    WriteMainMenu();
+      Scanner input = new Scanner(System.in);
+        System.out.print("Choice: ");
+         
+      int MenuChoice = input.nextInt();
+        
+      
+   
+            switch(MenuChoice){
+
+        case 1:
+       WriteMcDoMenu();
+          
+          
+      
+        break;
+        
+        case 2: 
+        
+        break;
+      
+        default:
+            System.out.println("Ye");
+    };
+            
+        
+       
+    }//End Main Function
+    
+    public static void  WriteMainMenu(){
+        System.out.println("=================Welcome to McDonalds=================");
+        System.out.println("1. Take Order \n2. Edit McDonals Menu \n3. Exit application ");
+
     }
     
-}
+    public static void WriteMcDoMenu(){
+        System.out.print("1-pc. Chicken Mcdo With Fries \n Php112.0 \n 1-pc  Scipy Chicken McDo w/ McSpaghetti "
+                +"\nPhp146.0 1-pc. Spicy Chicken McDo w/ Rice & Mushroom soup \n Php146.\n 1-pc. Chicken Mcdo w/ Spaghetti \n Php.144"
+                +"1-pc Chicken Mcdo w/ Rice & Mushroom Soup\n Php.144");
+    }
+    }
+    
+
+
